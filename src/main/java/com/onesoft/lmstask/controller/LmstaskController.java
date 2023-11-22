c2543028d518e6ed20acbd903beb693a5e35d80c
viji <moniviji313@gmail.com>
2023-11-22 15:49:58 +0530

    updatesuccessful

--- src/main/java/com/onesoft/lmstask/controller/LmstaskController.java
+++ src/main/java/com/onesoft/lmstask/controller/LmstaskController.java
@@ -1,9 +1,11 @@
 package com.onesoft.lmstask.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LmstaskController {
+	@GetMapping("value=/fetched")
 	public String pullData() {
 		return "hai all";
 		
